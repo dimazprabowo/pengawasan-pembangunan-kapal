598ede7a882cef6f7d59152824eff5a85d934118
Dimas Prabowo <dimasprabowo.cool@gmail.com>
2026-04-08 10:24:34 +0700

    update manajemen laporan: generate report

--- resources/views/livewire/laporan/laporan-show.blade.php
+++ resources/views/livewire/laporan/laporan-show.blade.php
@@ -179,8 +179,6 @@
                                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                     </svg>
-                                    <span x-show="!loading">Download</span>
-                                    <span x-show="loading" x-cloak>Mengunduh...</span>
                                 </a>
                             @endcan
                             @can('laporan_download')
@@ -195,8 +193,6 @@
                                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                     </svg>
-                                    <span wire:loading.remove wire:target="confirmDeleteDoc,deleteDoc">Hapus</span>
-                                    <span wire:loading wire:target="confirmDeleteDoc,deleteDoc">Menghapus...</span>
                                 </button>
                             @endcan
                         </div>
