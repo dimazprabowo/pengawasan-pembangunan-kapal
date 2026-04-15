fea88c1511cfc8c1178ac997dd09302a1101bb44
dimsky <dimazprabowo313@gmail.com>
2026-04-15 08:25:10 +0700

    update modal Lampiran harian in laporan mingguan

--- resources/views/components/laporan-mingguan/lampiran-harian-modal.blade.php
+++ resources/views/components/laporan-mingguan/lampiran-harian-modal.blade.php
@@ -82,6 +82,25 @@
                                         </div>
                                     @endif
                                     
+                                    {{-- Preview Button - Top Right --}}
+                                    @if($item['is_image'])
+                                        <button type="button" 
+                                            wire:click.stop="previewLampiranHarian({{ $item['id'] }})"
+                                            wire:loading.attr="disabled"
+                                            wire:target="previewLampiranHarian({{ $item['id'] }})"
+                                            class="absolute top-2 right-2 z-10 flex items-center justify-center w-8 h-8 bg-white dark:bg-gray-800 rounded-full shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors disabled:opacity-50"
+                                            title="Preview">
+                                            <svg wire:loading.class="hidden" wire:target="previewLampiranHarian({{ $item['id'] }})" class="w-4 h-4 text-gray-600 dark:text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
+                                            </svg>
+                                            <svg wire:loading wire:target="previewLampiranHarian({{ $item['id'] }})" class="animate-spin w-4 h-4 text-gray-600 dark:text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                            </svg>
+                                        </button>
+                                    @endif
+                                    
                                     {{-- Preview Image --}}
                                     <div class="aspect-video bg-gray-100 dark:bg-gray-800 relative overflow-hidden group"
                                          x-data="{ imageUrl: null, loading: false, loaded: false }"
@@ -128,19 +147,6 @@
                                                 </div>
                                             </div>
                                         @endif
-                                        
-                                        {{-- Overlay on hover --}}
-                                        <div class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-40 transition-all flex items-center justify-center opacity-0 group-hover:opacity-100">
-                                            <button type="button" 
-                                                wire:click.stop="previewLampiranHarian({{ $item['id'] }})"
-                                                class="px-3 py-1.5 bg-white dark:bg-gray-800 text-gray-900 dark:text-white rounded-lg text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors">
-                                                <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/>
-                                                </svg>
-                                                Preview
-                                            </button>
-                                        </div>
                                     </div>
 
                                     {{-- File Info --}}
