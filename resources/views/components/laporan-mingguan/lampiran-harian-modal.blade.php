02b959ca82caba4dc56b0c6ff40ee8c9a26514de
dimsky <dimazprabowo313@gmail.com>
2026-04-15 09:38:29 +0700

    update lampiran harian modal

--- resources/views/components/laporan-mingguan/lampiran-harian-modal.blade.php
+++ resources/views/components/laporan-mingguan/lampiran-harian-modal.blade.php
@@ -71,16 +71,19 @@
                                      wire:click="toggleLampiran({{ $item['id'] }})">
                                     
                                     {{-- Checkbox Badge --}}
-                                    @if($item['is_selected'])
-                                        <div class="absolute top-2 left-2 z-10 flex items-center justify-center w-6 h-6 bg-green-500 rounded-full shadow-lg">
-                                            <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <div class="absolute top-2 left-2 z-10 flex items-center justify-center w-6 h-6 bg-white dark:bg-gray-600 border-2 {{ $item['is_selected'] ? 'border-green-500 dark:border-green-400' : 'border-gray-300 dark:border-gray-500' }} rounded-full shadow"
+                                         wire:loading.class="animate-spin"
+                                         wire:target="toggleLampiran({{ $item['id'] }})">
+                                        @if($item['is_selected'])
+                                            <svg wire:loading.class="hidden" wire:target="toggleLampiran({{ $item['id'] }})" class="w-4 h-4 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M5 13l4 4L19 7"/>
                                             </svg>
-                                        </div>
-                                    @else
-                                        <div class="absolute top-2 left-2 z-10 flex items-center justify-center w-6 h-6 bg-white dark:bg-gray-600 border-2 border-gray-300 dark:border-gray-500 rounded-full shadow">
-                                        </div>
-                                    @endif
+                                        @endif
+                                        <svg wire:loading wire:target="toggleLampiran({{ $item['id'] }})" class="w-3 h-3 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                        </svg>
+                                    </div>
                                     
                                     {{-- Preview Button - Top Right --}}
                                     @if($item['is_image'])
